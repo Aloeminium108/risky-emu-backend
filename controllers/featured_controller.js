5b216021194806d55b8c147dfe08cf251242e575
Ben Dubroff <BLDubroff@gmail.com>
2023-07-12 20:46:08 -0400

    added skeletons of future controllers

--- controllers/featured_controller.js
+++ controllers/featured_controller.js
@@ -3,6 +3,10 @@ const db = require('../models')
 // const { fill in models here } = db
 // const { Op } = require('sequelize')
 
+featured.get('/', async(req, res) => {
+    res.send('hell yeah im featured!')
+})
+
 
 
 // exports
