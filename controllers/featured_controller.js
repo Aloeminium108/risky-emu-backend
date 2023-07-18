8693a422cd49ee44c87a31f0f0b259312c3c330f
Ben Dubroff <BLDubroff@gmail.com>
2023-07-17 20:46:51 -0400

    temp code

--- controllers/featured_controller.js
+++ controllers/featured_controller.js
@@ -1,33 +1,33 @@
-const featured = require('express').Router()
+const feature = require('express').Router()
 const db = require('../models')
-// const { fill in models here } = db
-// const { Op } = require('sequelize')
+const { featured, user_data } = db
+const { Op } = require('sequelize')
 
 
 //Find all featured programs
 
-featured.get('/', async(req, res) => {
+feature.get('/', async(req, res) => {
     res.send('hell yeah im featured!')
 })
 
 // Create a featured program || route may not be needed
 
-featured.post('/', (req, res) => {
+feature.post('/', (req, res) => {
     res.send('Got a POST request')
   })
 
 //Update a featured program
 
-featured.put('/featured', (req, res) => {
+feature.put('/featured', (req, res) => {
     res.send('Got a PUT request at /featured')
   })
 
 // Delete a featured program from the feature page?
 
-featured.delete('/featured', (req, res) => {
+feature.delete('/featured', (req, res) => {
     res.send('Got a DELETE request at /featured')
   })
 
   
 // exports
-module.exports = featured
+module.exports = featured_program
